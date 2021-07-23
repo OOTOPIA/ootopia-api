b299ae80e86150f1f00c47d9de243c662b1fb9d8
Victor Campelo <victor_campelo@outlook.com>
2021-07-23 09:05:31 -0300

    refactor (interests-tags): sql query directory for interest tags was changed

--- src/interests-tags/queries/new-tags.sql
+++ src/interests-tags/queries/new-tags.sql
@@ -1,5 +1,7 @@
+-- disables all current tags
 UPDATE interests_tags set active = false;
 
+-- adds new pt-br top tags
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('saude', 'top', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('autoconhecimento', 'top', 'pt-BR', 1, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('mindfulness', 'top', 'pt-BR', 2, true);
@@ -18,7 +20,7 @@ INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('en
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('economiasolidaria', 'top', 'pt-BR', 15, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('projetossociais', 'top', 'pt-BR', 16, true);
 
---pt-BR secondary tags
+-- adds new pt-BR secondary tags
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('natureza', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('agua', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('sustentabilidade', 'secondary', 'pt-BR', 0, true);
@@ -34,7 +36,7 @@ INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('re
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('povosnativos', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('moedassociais', 'secondary', 'pt-BR', 0, true);
 
-
+-- adds the rest of the new secondary tags
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('yoga', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('arte', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('chakras', 'secondary', 'pt-BR', 0, true);
@@ -158,62 +160,3 @@ INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('co
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('sintrópico', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('moedassociais', 'secondary', 'pt-BR', 0, true);
 INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('moedascomplementares', 'secondary', 'pt-BR', 0, true);
-
-
---pt-BR top tags
-
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Agroecologia', 'top', 'pt-BR', 11, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Alimentação', 'top', 'pt-BR', 3, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Ativismo', 'top', 'pt-BR', 10, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Consumo consciente', 'top', 'pt-BR', 4, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Economia solidária', 'top', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Educação', 'top', 'pt-BR', 5, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Energia limpa', 'top', 'pt-BR', 8, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Espiritualidade', 'top', 'pt-BR', 6, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Moedas sociais', 'top', 'pt-BR', 9, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Permacultura', 'top', 'pt-BR', 2, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Reciclagem', 'top', 'pt-BR', 1, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Projetos sociais', 'top', 'pt-BR', 7, true);
-
---pt-BR secondary tags
-
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Água', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Agrofloresta', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Orgânicos', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Bioconstrução', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Ecologia', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Natureza', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Preservação', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Regeneração', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Sustentabilidade', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Transição', 'secondary', 'pt-BR', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Povos tradicionais', 'secondary', 'pt-BR', 0, true);
-
---en-US top tags
-
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Agroecology', 'top', 'en-US', 11, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Food', 'top', 'en-US', 3, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Activism', 'top', 'en-US', 10, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Conscious consumption', 'top', 'en-US', 4, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Solidarity economy', 'top', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Education', 'top', 'en-US', 5, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Clean energy', 'top', 'en-US', 8, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Spirituality', 'top', 'en-US', 6, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Social currencies', 'top', 'en-US', 9, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Permaculture', 'top', 'en-US', 2, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Recycling', 'top', 'en-US', 1, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Social projects', 'top', 'en-US', 7, true);
-
---en-US secondary tags
-
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Water', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Agroforestry', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Organic', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Bioconstruction', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Ecology', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Nature', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Preservation', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Regeneration', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Sustainability', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Transition', 'secondary', 'en-US', 0, true);
-INSERT INTO interests_tags (name, type, language, tag_order, active) VALUES ('Traditional peoples', 'secondary', 'en-US', 0, true);
