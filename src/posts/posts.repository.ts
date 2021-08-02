cb063ad0160f117dcc1f74e7d158216f298500b9
Claudio Oliveira <claudio.oliveira@outlook.com.br>
2021-08-02 14:56:45 -0300

    fix

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -139,7 +139,7 @@ export class PostsRepository extends Repository<Posts>{
 
     async getPostsTimeline(filters, userId? : string) {
 
-        let where = "video_status = 'ready' AND deleted_at is null AND ";
+        let where = "(video_status = 'ready' OR (video_status is null AND type = 'image')) AND deleted_at is null AND ";
         const params = [];
         const perPage = 10;
         let limit = 'LIMIT ' + perPage;
