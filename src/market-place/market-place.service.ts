e2449829f1f3478fc6b7971ea447ee16994d1fd5
Claudio Oliveira <claudio_pg-sp@hotmail.com>
2021-10-14 09:49:38 -0300

    fix: renamed OOTOPIA Team to OOTOPIA

--- src/market-place/market-place.service.ts
+++ src/market-place/market-place.service.ts
@@ -109,7 +109,7 @@ export class MarketPlaceService {
   private mapper(marketPlaceProduct) {
     if (!marketPlaceProduct.userId) {
       marketPlaceProduct.userId = "ootopia";
-      marketPlaceProduct.userName = "OOTOPIA Team";
+      marketPlaceProduct.userName = "OOTOPIA";
       marketPlaceProduct.userPhotoUrl = "https://ootopia-files-staging.s3.sa-east-1.amazonaws.com/ootopia_marketplace_icon.png";
     }
     marketPlaceProduct.price = +marketPlaceProduct.price;
