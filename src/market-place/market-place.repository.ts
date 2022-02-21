f16c8607add63cc2be5b510abfa295103c89982f
Leandro Pereira <leandro-pereira2011@hotmail.com>
2022-02-21 11:07:09 -0300

    fix(*): added column in query

--- src/market-place/market-place.repository.ts
+++ src/market-place/market-place.repository.ts
@@ -63,7 +63,9 @@ export class MarketPlaceRepository extends Repository<MarketPlaceProducts>{
 
     async getById(id : string) {
         return camelcaseKeys(await getConnection().query(`
-            SELECT m.* FROM market_place_products m
+            SELECT m.*, u.fullname as "user_name"  
+            FROM market_place_products m
+            left join users u on u.id = m.user_id
             WHERE m.id = $1 and m.deleted_at is null
         `, [id]))[0];
     }
